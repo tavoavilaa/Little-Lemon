c0a4ee0062b52046a16cc4953bf00d090755c2d3
Gustavo Avila <info.gustavoavila@gmail.com>
2023-04-16 23:12:50 -0500

    first commit

---
+++ LittleLemonApp/LittleLemonApp/LittleLemonApp.swift
@@ -0,0 +1,14 @@
+
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    let persistenceController = PersistenceController.shared
+    
+    var body: some Scene {
+        WindowGroup {
+            Onboarding().environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
